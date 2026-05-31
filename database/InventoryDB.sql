86526a66d51bdd58fcb6b69882ac7667175a47d0
kindlyturnip <kindlyturnip17@student.ub.ac.id>
2026-05-31 23:00:52 +0700

    Menambahkan database dan project awal

---
+++ database/InventoryDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE InventoryDB;
+GO
+
+USE InventoryDB;
+GO
+
+CREATE TABLE Barang (
+    id_barang INT IDENTITY(1,1) PRIMARY KEY,
+    nama_barang VARCHAR(100) NOT NULL,
+    kategori VARCHAR(50) NOT NULL,
+    stok INT NOT NULL,
+    harga INT NOT NULL
+);
+GO
+
