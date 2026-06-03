365669e99210e324d876c2c947f2350381bfbfa0
kindlyturnip <kindlyturnip17@student.ub.ac.id>
2026-06-03 21:26:53 +0700

    Fix error

--- InventoryApp/src/koneksi/Koneksi.java
+++ InventoryApp/src/koneksi/Koneksi.java
@@ -17,9 +17,14 @@ public class Koneksi {
     public static Connection getConnection() {
         try {
 
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Inventory;encrypt=true;trustServerCertificate=true;";
-            String user = "basdat";
-            String pass = "123"; 
+            String url =
+                "jdbc:sqlserver://LAPTOP-GMPEQVN5;"
+                + "databaseName=InventoryDB;"
+                + "encrypt=false;"
+                + "trustServerCertificate=true";
+
+            String user = "Db_Con";
+            String pass = "123";  
 
             conn = DriverManager.getConnection(url, user, pass);
 
